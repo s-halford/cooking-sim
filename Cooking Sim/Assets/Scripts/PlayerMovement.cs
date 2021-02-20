a69e4533c39f8cfd77c5a12f906e6d9fd618402b
Scott Halford <>
2021-02-20 14:46:46 -0600

    Add character, set up basic movement and animations
    *Character sprites from 'Tiny RPG-Forest' asset.  (https://assetstore.unity.com/packages/2d/characters/tiny-rpg-forest-114685)

---
+++ Cooking Sim/Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour
+{ 
+    public float moveSpeed = 5f;
+    private Rigidbody2D rb;
+    private Animator anim;
+    private Vector2 movement;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+        anim = GetComponent<Animator>();
+    }
+
+    void Update()
+    {
+        movement.x = Input.GetAxisRaw("Horizontal");
+        movement.y = Input.GetAxisRaw("Vertical");
+
+        if (movement != Vector2.zero)
+        {
+            anim.SetFloat("Horizontal", movement.x);
+            anim.SetFloat("Vertical", movement.y);
+
+            // If we're moving left set xScale to -1 to horizontally flip sideways-facing sprites
+            float xScale = movement.x < 0 ? -1f : 1f;
+            transform.localScale = new Vector3(xScale, 1f, 1f);
+        }
+
+        anim.SetFloat("Speed", movement.sqrMagnitude);
+
+    }
+
+    private void FixedUpdate()
+    {
+        rb.MovePosition(rb.position + movement * moveSpeed * Time.fixedDeltaTime);
+    }
+
+
+
+}
